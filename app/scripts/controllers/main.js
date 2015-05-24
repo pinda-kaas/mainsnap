4fefcbcae93c61cf54b2d5f5d90d489377340d75
Teddy <Teddy@mail.com>
2015-05-24 20:48:28 +1000

    more tests

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -4,17 +4,19 @@ app.controller('MainCtrl', function ($scope) {
 
   $scope.playerCards = [];
   $scope.cpuCards = [];
-  $scope.centrePileCards= [];
-
-  $scope.cpuCards = [];
-
-  $scope.playerTurn=null;
+  $scope.centrePileCards = [];
+  $scope.playerTurn = null;
 
   ///start of game
   //
   $scope.dealCards = function () {
+
+    //reset all cards
     $scope.playerCards = [];
     $scope.cpuCards = [];
+    $scope.centrePileCards = [];
+
+    console.log('dealcards start');
 
     var nrs = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
     var suits = ['c', 'h'];
@@ -35,26 +37,50 @@ app.controller('MainCtrl', function ($scope) {
       });
     });
 
+    //debugger;
+
+    console.log('player cards len', $scope.playerCards.length);
+    console.log('cpu cards len', $scope.cpuCards.length);
   }
 
-  $scope.switchPlayerTurn = function () {
+  $scope.startGame = function () {
+    //choose random player
     $scope.playerTurn = Math.random() > 0.5 ? 1 : 0;
+
+    //when the player is cpu trigger a pickcard
+    if ($scope.playerTurn) {
+      $scope.pickCard();
+    }
   }
 
   //playerTurn p (integer ) picks card from their pile and places it on centre pile
   $scope.pickCard = function () {
-
     //if player
-    if ($scope.playerTurn==0)
-    {
-      $scope.centrePileCards.push($scope.playerCards[0]);
-      $scope.playerCards.splice(0,1)
+    debugger;
+    if (!$scope.playerTurn && $scope.playerCards.length > 0) {
+      $scope.centrePileCards.unshift($scope.playerCards[0]);
+      $scope.playerCards.splice(0, 1)
     }
     else
     //cpu
-    {
-      $scope.centrePileCards.push($scope.cpuCards[0]);
-      $scope.cpuCards.splice(0,1)
+    if ($scope.cpuCards.length > 0) {
+      $scope.centrePileCards.unshift($scope.cpuCards[0]);
+      $scope.cpuCards.splice(0, 1)
     }
   }
+
+  //switch turns
+  $scope.switchTurns = function () {
+    $scope.playerTurn = !$scope.playerTurn;
+    console.log($scope.centrePileCards.length);
+    //when the player is cpu trigger a pickcard
+    if ($scope.playerTurn) {$scope.pickCardCpu();}
+  }
+
+  //pickcard by cpu
+  $scope.pickCardCpu=function(){
+    $scope.pickCard();
+    $scope.switchTurns();
+  }
+
 });
--- test/spec/controllers/main.js
+++ test/spec/controllers/main.js
@@ -3,9 +3,7 @@ describe('main ctrl tests', function() {
   var scope, createController;
 
   beforeEach(inject(function ($rootScope, $controller ) {
-
     scope = $rootScope.$new();
-
     createController = function() {
       return $controller('MainCtrl', {
         '$scope': scope
@@ -13,13 +11,13 @@ describe('main ctrl tests', function() {
     };
   }));
 
-  it('should have a 26 cards for player', function() {
+  it('should have a 26 cards for player after dealing', function() {
     var controller = createController();
     scope.dealCards();
     expect(scope.playerCards.length).toBe(26);
   });
 
-  it('should have a 26 cards for cpu', function() {
+  it('should have a 26 cards for cpu after dealing', function() {
     var controller = createController();
     scope.dealCards();
     expect(scope.cpuCards.length).toBe(26);
@@ -31,7 +29,6 @@ describe('main ctrl tests', function() {
     scope.dealCards();
 
     var allCards = scope.cpuCards.concat(scope.playerCards);
-
     var allHearts = _.where(allCards,{suit:"h"});
     var allSpades = _.where(allCards,{suit:"s"});
     var allDaimonds = _.where(allCards,{suit:"d"});
@@ -45,23 +42,76 @@ describe('main ctrl tests', function() {
 
   it('should player be either 0 or 1 random',function(){
     var controller = createController();
-    scope.switchPlayerTurn();
+    scope.startGame();
     var result=scope.playerTurn;
     var bool = (result==1 || result==0);
     expect(bool).toBe(true);
-  })
+  });
 
-  it('should pick one card from own pile and add to centrepile',function(){
+  it('should pick one card from own pile and add to centrepile as player after dealing',function(){
     var controller = createController();
     scope.dealCards();
     scope.playerTurn=0;
     var oldLength = scope.playerCards.length;
     var oldLengthCentre=scope.centrePileCards.length;
-    console.log('oldLength',oldLengthCentre);
+    console.log('oldLength player',oldLength);
+    console.log('oldLength centre',oldLengthCentre);
     scope.pickCard();
     expect(scope.playerCards.length).toBe(oldLength-1);
     expect(scope.centrePileCards.length).toBe(oldLengthCentre+1);
-  })
+    console.log(' new length player',scope.playerCards.length);
+    console.log('new length centre',scope.centrePileCards.length);
+  });
+
+  it('should pick one card from own pile and add to centrepile as player starting with 1 card',function(){
+    var controller = createController();
+    scope.playerCards.push({number:2,suit:"s"});
+    scope.playerTurn=0;
+    scope.pickCard();
+    expect(scope.playerCards.length).toBe(0);
+    expect(scope.centrePileCards.length).toBe(1);
+    expect(scope.centrePileCards[0].number).toBe(2);
+  });
+
+
+  it('should pick one card from own pile and add to centrepile as cpu after dealing',function(){
+    var controller = createController();
+    scope.dealCards();
+    scope.playerTurn=1;
+    var oldLength = scope.cpuCards.length;
+    var oldLengthCentre=scope.centrePileCards.length;
+    scope.pickCard();
+    expect(scope.cpuCards.length).toBe(oldLength-1);
+    expect(scope.centrePileCards.length).toBe(oldLengthCentre+1);
+
+  });
+
+  it('should pick one card from own pile and add to centrepile as cpu starting with 1 card',function(){
+    var controller = createController();
+    scope.cpuCards.push({number:5,suit:"h"});
+    scope.playerTurn=1;
+    scope.pickCard();
+    expect(scope.cpuCards.length).toBe(0);
+    expect(scope.centrePileCards.length).toBe(1);
+    expect(scope.centrePileCards[0].suit).toBe("h");
+  });
+
+
+  it('should pick one card from own pile and add to centrepile automatically when cpu',function(){
+    var controller = createController();
+    spyOn(scope,'pickCard')
+    scope.playerTurn=0;
+    scope.switchTurns();
+    expect(scope.pickCard).toHaveBeenCalled();
+  });
+
+  it('should switch turns',function(){
+    var controller = createController();
+    scope.playerTurn=0;
+    scope.switchTurns();
+    console.log('playerTurn',scope.playerTurn);
+    expect(scope.playerTurn).toBe(true);
+  });
 
 
 
